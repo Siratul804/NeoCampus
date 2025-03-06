45e89f64c454d7ca4cd270d6745d1e563d9a8c66
Siratul Islam <islamsiratul@gmail.com>
2025-03-06 08:21:57 +0600

    feat: added realtime on triiger button navbar

--- app/page.js
+++ app/page.js
@@ -1,69 +1,15 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { socket } from "../socket";
-import { SidebarPage } from "@/components/Sidebar";
-
-import { useUser } from "@clerk/nextjs";
+// import { useUser } from "@clerk/nextjs";
 import Dashboard from "./(main)/dashboard/page";
 
 export default function Home() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
-  const [notification, setNotification] = useState("");
-
-  const { user: clerkUser, isLoaded } = useUser();
+  // const { user: clerkUser, isLoaded } = useUser();
 
   // console.log(clerkUser?.id);
   // console.log(clerkUser?.imageUrl);
 
-  useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
-
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
-
-      socket.io.engine.on("upgrade", (transport) => {
-        setTransport(transport.name);
-      });
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-
-    socket.on("notification", (message) => {
-      setNotification(message);
-    });
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-      socket.off("notification");
-    };
-  }, []);
-
-  const handleClick = () => {
-    socket.emit("triggerNotification");
-  };
-
   return (
     <div>
-      {/* <p>Status: {isConnected ? "connected" : "disconnected"}</p>
-      <p>Transport: {transport}</p>
-      <button onClick={handleClick} className=" bg-amber-200 rounded ">
-        Trigger Notification
-      </button>
-      {notification && <p>Notification: {notification}</p>} */}
-      {/* <SidebarPage/> */}
-      <Dashboard/>
+      <Dashboard />
     </div>
   );
 }
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,15 +1,59 @@
-import MindAr from "@/app/components/MindAr";
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { socket } from "../../socket";
 
 function page() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+  const [notification, setNotification] = useState("");
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    socket.on("notification", (message) => {
+      setNotification(message);
+    });
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("notification");
+    };
+  }, []);
+
+  const handleClick = () => {
+    socket.emit("triggerNotification");
+  };
+
   return (
     <>
-      <MindAr />
-      <div>
-        <h2>Embedded AR Page</h2>
-        <a href="/ar.html" target="_blank" rel="noopener noreferrer">
-          Open AR Page
-        </a>
-      </div>
+      <p>Status: {isConnected ? "connected" : "disconnected"}</p>
+      <p>Transport: {transport}</p>
+      <button onClick={handleClick} className=" bg-amber-200 rounded ">
+        Trigger Notification
+      </button>
+      {notification && <p>Notification: {notification}</p>}
     </>
   );
 }
