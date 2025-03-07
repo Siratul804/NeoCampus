268410ec5d579b810af95eb6779b840e99a2af7a
atik65 <atikhasan801@gmail.com>
2025-03-07 14:14:24 +0600

    fix: sidebar padding has been fixed

--- app/components/ClientLayout.jsx
+++ app/components/ClientLayout.jsx
@@ -4,7 +4,6 @@ import { Navbar } from "@/components/Navbar";
 import { SidebarPage } from "@/components/Sidebar";
 import { usePathname } from "next/navigation";
 
-
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
   const isAdminRoute = pathname.startsWith("/admin");
@@ -14,7 +13,7 @@ export default function ClientLayout({ children }) {
       {!isAdminRoute && <SidebarPage />}
       <div className="flex flex-col flex-1 overflow-hidden">
         {!isAdminRoute && <Navbar />}
-        <main className="flex-1 overflow-y-auto px-4 py-0">{children}</main>
+        <main className="flex-1 overflow-y-auto px-0 py-0">{children}</main>
       </div>
     </div>
   );
